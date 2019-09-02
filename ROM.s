d3f32d2d6b9a18361be47137d1dae9f140f94508
Olivier Jan <ojanhk@gmail.com>
2019-09-02 15:15:37 +0200

    Update ROM.s
    Define the master file to build the whole ROM.

--- ROM.s
+++ ROM.s
@@ -1,5 +1,50 @@
-;******************************************************************************************
-;*                                                                                        *
-;*                        Master file for ROM generation                                  *
-;*                                                                                        *
-;******************************************************************************************
+*------------------------------------------------------------------------------------------
+*--                                                                                      --
+*--                        Master file for ROM generation                                --
+*--                                                                                      --
+*------------------------------------------------------------------------------------------
+
+
+
+ROMSTART            EQU     $B000 ; 
+BASICSTART          EQU     $B000 ;
+MONITORSTART        EQU     $D900 ;
+BIOSSTART           EQU     $FE00 ;
+ACIASTART           EQU     $A000 ;
+ACIATYPE            EQU     ACIA6551 ;
+;ACIATYPE           EQU     ACIA6850 ;
+ROMNAME             EQU     OJROM.bin ;
+
+
+                    DSK     ROMNAME
+                    ORG     ROMSTART
+                    TYP     $06
+                    
+                    DO BASICSTART
+                    
+                    ORG     BASICSTART
+                    PUT     BASIC/Basic.s
+                    
+                    FIN
+                    
+                    DO MONITORSTART
+                    
+                    ORG     MONITORSTART
+                    PUT     MONITOR/jmon.s
+                    PUT     MONITOR/disasm.s
+                    PUT     MONITOR/miniasm.s
+                    PUT     MONITOR/trace.s
+                    PUT     MONITOR/info.s
+                    PUT     MONITOR/delay.s
+                    
+                    FIN
+                    
+                    ORG     BIOSSTART
+                    PUT     BIOS/Init.s
+                    PUT     BIOS/Serial.s
+                    PUT     BIOS/Vectors.s
+                    
+                    
+END
+
+
