d0dfb51f00b211263519c58e09aa686c77ab9f6a
Olivier Jan <ojanhk@gmail.com>
2019-09-02 15:36:43 +0200

    Update Vectors.s

--- BIOS/Vectors.s
+++ BIOS/Vectors.s
@@ -6,12 +6,7 @@
 *-------------------------------------------------------------------------------
 
 
-
-          DSK	  Vectors.bin
-          ORG     $FFFA
-          TYP     $06
-
-RESET     EXT                   ; Define External reference to RESET.
+          
           DA     RESET          ; NMI Vector
           DA     RESET          ; RESET Vector
           DA     $03F0          ; IRQ Vector
